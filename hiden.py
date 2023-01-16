5950dc93dc54b74f5bba447979490e5255123d40
udoy56 <115587072+udoy56@users.noreply.github.com>
2023-01-17 03:43:12 +0600

    Create hiden.py

---
+++ hiden.py
@@ -0,0 +1,109 @@
+The Uncensored Hidden Wiki
+
+
+
+
+
+What Is The Uncensored Hidden Wiki ?
+
+
+
+
+
+
+
+A few years ago, except The Hidden Wiki at TheHiddenWiki.com there was also a hidden wiki on the darknet as a hidden service called the uncensored hidden wiki.
+
+
+Many of you may be wondering what the uncensored is about.
+
+
+
+It basically means it hosted many links to illegal pornography aka hard candy or CP.
+
+
+
+Those sites often get shut down by law enforcement so people looking for that kind of site, showing indecent pictures of girls or jailbait, had to make a catalog or list of those sites at this uncensored hidden wiki.
+
+
+
+But that wiki has since been shut down like so many hidden wiki sites before. But of course, new sites are always getting created.
+
+
+
+You should not search for those kind of sites on the dark web, as it is very high risk of getting caught.
+
+
+
+We at The Hidden Wiki only list dark web links which are legal to browse, but keep in mind that buying drugs online is not legal, and we do not advise to do it, but what ever you are going to do is up to you at the end.
+
+
+
+The dark web is a deep place with all kinds of sites, and those hard candy sites are definitely the worst examples. So stay away.
+
+
+
+There have been rumors of sites called red rooms or other sadistic themes, but those are all not real. People just make them as a joke or fun site, often with a very bad taste but not real anyway.
+
+
+
+ 
+
+Different Hidden Wiki Mirrors
+
+
+
+ 
+
+
+
+As we already said there have been dozens of hidden wiki like sites or mirrors of the hidden wiki over the years since the dark web exists, but not many of them are complete or trustworthy and most list questionable links.
+
+
+
+Make sure to bookmark some good dark web links lists for your dark web browsing.
+
+
+
+Except this site, some good examples are TorLinks or OnionList, which you can find on the main page.
+
+
+
+in 2020, the dark web is over 10 years old, 100s of dark web sites have since then been created and gone down sometimes only days later.
+
+
+
+We always scan the dark web for new sites so we can provide you a good overview over what exists on the dark web and how to access the dark web.
+
+
+
+We hope you have some fun in browsing all the sites, it took is months if not years to find them all for you.
+
+
+
+We will grow this list and try to make it the reference for dark web newbies and experts at the same time, while making sure we do not list bad sites or clone sites in our directory.
+
+
+Hidden Wiki Hard Candy Jailbait
+
+
+
+ 
+
+How To Add Links To The Hidden Wiki
+
+
+
+ 
+
+
+
+Some hidden wiki sites had been open to public editing, but we decided against this since this only gives many spam sites a place.
+
+
+
+But if you want to add your site to The Hidden Wiki then go to our contact page and send us a link.
+
+
+
+But we will not add any obviously spam or scam sites, don’t even try. We check all sites before we list them on the wiki. 
